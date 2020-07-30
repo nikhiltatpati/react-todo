653619f204eb8615010eec0dfdfa7d829e847c06
nikhil <nikhiltatpati11@gmail.com>
2020-07-30 13:04:16 +0530

    Updates

---
+++ precache-manifest.2186da850db816697b763d69b49125c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60bb85ef80a040248b3166dbde22e27",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "561c3000514fe4a9a5e8",
+    "url": "/react-todo/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "cf8bcef7adf04462925c",
+    "url": "/react-todo/static/css/main.47b2c8f4.chunk.css"
+  },
+  {
+    "revision": "561c3000514fe4a9a5e8",
+    "url": "/react-todo/static/js/2.cf75e4c2.chunk.js"
+  },
+  {
+    "revision": "cf8bcef7adf04462925c",
+    "url": "/react-todo/static/js/main.35f2e78d.chunk.js"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  }
+]);
